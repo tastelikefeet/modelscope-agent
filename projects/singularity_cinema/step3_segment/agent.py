7060cd9196fecf22abd788f8147a0ace4e358eb3
tastelikefeet <yuze.zyz@alibaba-inc.com>
2025-11-16 10:25:50 +0800

    fix

--- projects/singularity_cinema/step3_segment/agent.py
+++ projects/singularity_cinema/step3_segment/agent.py
@@ -22,14 +22,6 @@ class Segment(LLMAgent):
     * Consider the color style across all storyboards comprehensively, for example, all purple style, all deep blue style, etc.
         - Consider more colors like white, black, dark blue, dark purple, dark orange, etc, which will make your design elegant, avoid using light yellow/blue, which will make your animation look superficial, DO NOT use grey color, it's not easy to read
     * Use less stick man unless the user wants to, to prevent the animation from being too naive, try to make your effects more dazzling/gorgeous/spectacular/blingbling
-    * Manim animation may contain one or more images, these images come from user's documentation, or a powerful text-to-image model (same way with the generated background images)
-
-- If the user's documentation contains any images, the information will be given to you:
-    * The image information will include content description, size(width*height) and filename
-    * Select useful images in each segment and reference the filename in the `user_image` field
-
-- User-provided images may be insufficient. Trust text-to-image models to generate additional images for more visually compelling videos
-    * Output image generation requirements and the generated filenames(with .png format) in `foreground` field
 
 - Each of your storyboard panels should take about 5 seconds to 10 seconds to read at normal speaking speed. Avoid the feeling of frequent switching and static
     * If a storyboard panel has no manim animation, it should not exceed 5s
@@ -67,30 +59,11 @@ An example:
         "index": 1, # index of the segment, start from 1
         "content": "Now let's explain...",
         "background": "An image describe... color ... (your detailed requirements here)",
-        "manim": "The animation should ..., use images to... ",
-        "user_image": [
-            "user_image1.jpg",
-            "user_image2.jpg"
-        ]
-        "foreground": [
-            "An image describe... color ... (your detailed requirements here)",
-            ...
-        ],
+        "manim": "The animation should ... draw component ...",
     },
     ...
 ]
 ```
-
-An example of image structures given to the manim LLM:
-```json
-[
-    {
-        "file_path": "foreground_images/1.jpg",
-        "size": "2000*2000",
-        "description": "The image contains ..."
-    },
-    ...
-]
 ```
 
 Now begin:""" # noqa
@@ -122,26 +95,9 @@ Now begin:""" # noqa
         with open(os.path.join(self.work_dir, 'topic.txt'), 'r') as f:
             topic = f.read()
 
-        new_image_info = 'No images offered.'
-        name_mapping = {}
-        if os.path.exists(os.path.join(self.work_dir, 'image_info.txt')):
-            with open(os.path.join(self.work_dir, 'image_info.txt'), 'r') as f:
-                image_info = f.readlines()
-
-            image_info = [image.strip() for image in image_info if image.strip()]
-            for i, info in enumerate(image_info):
-                info = json.loads(info)
-                filename = info['filename']
-                new_filename = f'user_image_{i}.png'
-                name_mapping[new_filename] = filename
-                info['filename'] = new_filename
-
-            new_image_info = json.dumps(image_info, ensure_ascii=False)
-
         query = (
             f'Original topic: \n\n{topic}\n\n'
             f'Original script：\n\n{script}\n\n'
-            f'User offered images: \n\n{new_image_info}\n\n'
             f'Please finish your animation storyboard design:\n')
         messages = await super().run(query, **kwargs)
         response = messages[-1].content
@@ -150,13 +106,7 @@ Now begin:""" # noqa
         elif '```' in response:
             response = response.split('```')[1].split('```')[0]
         segments = json.loads(response)
-        for i, segment in enumerate(segments):
-            user_images = segment.get('user_image', [])
-            new_user_images = []
-            for image in user_images:
-                if image in name_mapping:
-                    new_user_images.append(name_mapping[image])
-            segment['user_image'] = new_user_images
+        segments = await self.add_images(segments, topic, script, **kwargs)
 
         for i, segment in enumerate(segments):
             assert 'content' in segment
@@ -170,13 +120,21 @@ Now begin:""" # noqa
             f.write(json.dumps(segments, indent=4, ensure_ascii=False))
         return messages
 
-    async def add_images(self, segments, topic, script, new_image_info, **kwargs):
+    async def add_images(self, segments, topic, script, **kwargs):
 
         system = """你是一个动画短视频分镜辅助设计师。你的职责是协助分镜设计师为分镜添加前景图片。你会被一个给与一个分镜设计草案，和一些图片列表，该图片列表来自用户输入，可以自由挑选使用。
 下面你需要选择两类图片：
 
+Manim animation may contain one or more images, these images come from user's documentation, or a powerful text-to-image model (same way with the generated background images)
+- If the user's documentation contains any images, the information will be given to you:
+    * The image information will include content description, size(width*height) and filename
+    * Select useful images in each segment and reference the filename in the `user_image` field
+
+- User-provided images may be insufficient. Trust text-to-image models to generate additional images for more visually compelling videos
+    * Output image generation requirements and the generated filenames(with .png format) in `foreground` field
+    
 1. 来自用户的图片，可以直接使用
-2. 你认为的可以增加短视频效果的图片，你需要给出生成图片的具体要求,一个收信任的文生图模型会帮你生成它
+2. 你认为的可以增加短视频效果的图片，你需要给出生成图片的具体要求,一个收信任的文生图模型会帮你生成它，生成的图片均为正方形
 3. 修改对应分镜的manim字段，该字段用于后续的manim动画生成的指导意见，修改该字段使后续manim生成大模型清楚如何使用你的这些图片
 4. 每个分镜使用的图片数量不必相同，也可以不使用图片
 5. 仔细分析用户提供的图片信息，尽可能使用它们
@@ -202,28 +160,68 @@ Now begin:""" # noqa
 ]
 ```
 
+An example of image structures given to the manim LLM:
+```json
+[
+    {
+        "file_path": "foreground_images/1.jpg",
+        "size": "2000*2000",
+        "description": "The image contains ..."
+    },
+    ...
+]
+
 注意:
 * 你的返回值中不必要包括content和background，这些信息不需要你关心
 * 你的返回长度应当和源分镜长度相同。如果不需要图片则返回空的user_image和foreground列表
 
 现在开始：
 """
+        new_image_info = 'No images offered.'
+        name_mapping = {}
+        if os.path.exists(os.path.join(self.work_dir, 'image_info.txt')):
+            with open(os.path.join(self.work_dir, 'image_info.txt'), 'r') as f:
+                image_info = f.readlines()
+
+            image_info = [image.strip() for image in image_info if image.strip()]
+            for i, info in enumerate(image_info):
+                info = json.loads(info)
+                filename = info['filename']
+                new_filename = f'user_image_{i}.png'
+                name_mapping[new_filename] = filename
+                info['filename'] = new_filename
+
+            new_image_info = json.dumps(image_info, ensure_ascii=False)
+
         query = (
             f'Original topic: \n\n{topic}\n\n'
             f'Original script：\n\n{script}\n\n'
+            f'Original segments：\n\n{json.dumps(segments, ensure_ascii=False, indent=4)}\n\n'
             f'User offered images: \n\n{new_image_info}\n\n'
             f'Please finish your images design:\n')
         messages = [
             Message(role='system', content=system),
             Message(role='user', content=query),
         ]
-        messages = self.llm.generate(messages)
-        response = messages[-1].content
+        message = self.llm.generate(messages, **kwargs)
+        response = message.content
         if '```json' in response:
             response = response.split('```json')[1].split('```')[0]
         elif '```' in response:
             response = response.split('```')[1].split('```')[0]
         _segments = json.loads(response)
 
+        for i, segment in enumerate(_segments):
+            user_images = segment.get('user_image', [])
+            new_user_images = []
+            for image in user_images:
+                if image in name_mapping:
+                    new_user_images.append(name_mapping[image])
+            segment['user_image'] = new_user_images
+
         assert len(_segments) == len(segments)
+        for segment, _segment in zip(segments, _segments):
+            segment.update(_segment)
+
+        return segments
 
