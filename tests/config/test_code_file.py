e643285eb75598fb881a353e7cc4516c13c6bf69
tastelikefeet <yuze.zyz@alibaba-inc.com>
2025-12-17 18:25:07 +0800

    add ut

--- tests/config/test_code_file.py
+++ tests/config/test_code_file.py
@@ -1,27 +1,30 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import unittest
 
-from modelscope.utils.test_utils import test_level
+from ms_agent.config import Config
 from omegaconf import DictConfig
 
-from ms_agent.config import Config
+from modelscope.utils.test_utils import test_level
 
 
 class TestConfig(unittest.TestCase):
 
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
     def test_safe_get_config(self):
-        config = DictConfig({
-            "tools" : {
-                "file_system" : {
-                    "system_for_abbreviations" : "test"
+        config = DictConfig(
+            {'tools': {
+                'file_system': {
+                    'system_for_abbreviations': 'test'
                 }
-            }
-        })
-        self.assertEqual("test", Config.safe_get_config(config, 'tools.file_system.system_for_abbreviations'))
+            }})
+        self.assertEqual(
+            'test',
+            Config.safe_get_config(
+                config, 'tools.file_system.system_for_abbreviations'))
         delattr(config.tools, 'file_system')
-        self.assertTrue(Config.safe_get_config(config, 'tools.file_system.system_for_abbreviations') is None)
-
+        self.assertTrue(
+            Config.safe_get_config(
+                config, 'tools.file_system.system_for_abbreviations') is None)
 
 
 if __name__ == '__main__':
